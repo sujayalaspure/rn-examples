d95bab67e2c36625e38ce926d4ec25c0551937e6
Sujay Alaspure <alaspuresujay@gmail.com>
2023-03-22 16:30:17 +0530

    fix: removed JIRA commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  plugins: ['commitlint-plugin-jira-rules'],
-  extends: ['jira'],
-  // extends: ['@commitlint/config-conventional'],
-  rules: {
-    'jira-task-id-project-key': [2, 'always', ['PROJ', 'PROJ2']],
-  },
+  // plugins: ['commitlint-plugin-jira-rules'],
+  // extends: ['jira'],
+  extends: ['@commitlint/config-conventional'],
+  // rules: {
+  //   'jira-task-id-project-key': [2, 'always', ['PROJ', 'PROJ2']],
+  //   'jira-commit-status-case': [0],
+  // },
 };
