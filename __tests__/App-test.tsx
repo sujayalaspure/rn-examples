a0e1d57ff5c45ddc85e55e37392a1143a9c50d0b
Sujay Alaspure <alaspuresujay@gmail.com>
2023-05-09 22:00:07 +0530

    fix: test case failing due to reanimated

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react-native';
 import React from 'react';
 import App from '../App';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('react-native-reanimated/lib/commonjs/reanimated2/jestUtils').setUpTests();
+
+// eslint-disable-next-line global-require
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
 
 describe('App', () => {
   it('should renders correctly', () => {
